d490f1c889498384b8174e1a7900570eda985bb4
SangYeon Yoon <yskite09@gmail.com>
2024-03-05 21:59:44 +0900

    cpp

--- Prog_Assignment01/prob05.cpp
+++ Prog_Assignment01/prob05.cpp
@@ -4,18 +4,18 @@ using namespace std;
 int main() 
 {
     int N, num;
-    cin >> N;                   // 정수 개수
+    cin >> N;                           // 정수 개수
     
     int smallest = -1;
 
     for (int i = 0; i < N; i ++)
     {
-        cin >> num;
+        cin >> num;                     // 정수 입력
         
-        if (num < 0)
-            continue;
-        else if (smallest == -1)
-            smallest = num;   
+        if (num < 0)                    // num이 음의 정수라면
+            continue;                   // 이번 반복을 종료 후 다음 반복 시작
+        else if (smallest == -1)       
+            smallest = num;             
         else if (num < smallest)
             smallest = num; 
     }
