143b573ca980dfa9ca4b00509a5716b78bf429f2
SangYeon Yoon <yskite09@gmail.com>
2024-03-16 00:29:30 +0900

    cpp

--- Prog_Assignment01/prob23.cpp
+++ Prog_Assignment01/prob23.cpp
@@ -7,40 +7,45 @@ int main()
     int data[MAX] = {0};
     int index = 0;
     int t;
-    for (int i = 0; ; i++)
+    
+    // 입력된 숫자 전체를 검사하여 t와 중복되었는지 판정
+    // i == 입력해야되는 배열의 인덱스
+    for (int i = 0; i < MAX; i++)
     {
         cin >> t;
-        int stop = 0;
-        //프로그램 종료
         if (t == -1)
             break;
-
-        //반복되는지 확인
-        for (int k = 0; k < i; i++)
+        
+        // 중복 값이 나올 때
+        int duplicate = 0;
+        if (i > 0)
         {
-            if (data[i] == t)
+            for (int n = i-1; n >= 0; n--)
             {
-                cout << "duplicate" << endl;
-                stop++;
-                break;
+                if (data[n] == t)
+                {
+                    duplicate++;
+                }
             }
         }
-        if (stop > 0)
+
+        if (duplicate == 1)
         {
             i--;
+            cout << "duplicate" << endl;
             continue;
         }
 
-        // 반복이 안되면 정렬
-        int j = i - 1;
-        
+        // 중복 값이 나오지 않았을 때
+        int j = i-1;                                            // 반복 당시 제일 끝 인덱스: i-1    
+          
         while (j >= 0 && data[j] > t)                           // 제일 끝 인덱스가 0보다 크거나 같고 data[j]가 t보다 클 때,
-        {   
+        {
             data[j+1] = data[j];                                // data[j]의 뒤의 인덱스에 자기의 값을 대입하고
             j--;                                                // j의 값을 1 낮춘다.
         }
-    
-        data[j+1] = t;
+        data[j+1] = t;                                          // 조건을 만족하는 인덱스 j+1 위치에 t를 대입
+
         for (int k = 0; k <= i; k++)
             cout << data[k] << " ";
         cout << endl;
