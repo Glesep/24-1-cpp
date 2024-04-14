ec8d9e66fba44aa3d580477100f9fd3bfec727aa
SangYeon Yoon <yskite09@gmail.com>
2024-04-14 20:23:27 +0900

    1

--- Group_Activity03/03.cpp
+++ Group_Activity03/03.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
 #include <string>
 using namespace std;
-string &concat(string str1, string str2) {
-    string combined = str1 + str2;
+string &concat(string str1, string str2, string& combined) {
+    combined = str1 + str2;
     return combined;
 }
 int main() {
     string first = "How fun";
     string second = " is C++ !";
-    string merged = concat(first, second);
+    string combined = "";
+    // 참조 딱지 붙여주면 같은 변수가 된다 생각하자
+    string merged = concat(first, second, combined);
     cout << merged << endl;
     return 0;
 }
---
+++ Group_Activity03/06.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+class A {
+public:
+    int a, b;
+};
+int main() {
+    A x, y;
+    x.a = 1, x.b = 2;
+    y = x;
+    // 객체끼리 비교 안됨
+    if (x==y)
+        cout << "Equal";
+    else
+        cout << "Not equal";
+}
